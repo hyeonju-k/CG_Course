1443ad2a62b2ddb232b0b0e619b239bb14811088
HyeonJu <hj950000@ajou.ac.kr>
2020-05-03 15:21:55 +0900

    HW3_CUBE add Math.random() to change color

--- WebGL/cube.js
+++ WebGL/cube.js
@@ -74,275 +74,281 @@ var vertexData = [
 
   // You need to make here!
 ];
-var elementData = [0, 1, 2, 3, 4, 5];
+var elementData = [0, 1];
 
 function createCubePos(sx, sy, sz) {
   // 1.0 1.0 1.0 쓰면
   //0.5 0.5 0.5 r g b a 36 * 7 elements 1차원
+
+  var color = new Array(); // color 항상 다르게
+  for (let i = 0; i < 4 * 36; i++) {
+    color[i] = Math.random();
+  }
+
   var vertexArray = [
     sx / 2,
     sy / 2,
     sz / 2,
-    0.0,
-    0.2,
-    0.4,
-    0.8, // v0
+    color[0],
+    color[1],
+    color[2],
+    color[3], // v0
     -sx / 2,
     sy / 2,
     sz / 2,
-    0.7,
-    0.0,
-    0.3,
-    0.5, // v1
+    color[4],
+    color[5],
+    color[6],
+    color[7], // v1
     -sx / 2,
     -sy / 2,
     sz / 2,
-    0.7,
-    0.9,
-    0.0,
-    0.1, // v2
+    color[8],
+    color[9],
+    color[10],
+    color[11], // v2
 
     sx / 2,
     sy / 2,
     sz / 2,
-    0.2,
-    0.3,
-    0.4,
-    0.5, // v0
+    color[12],
+    color[13],
+    color[14],
+    color[15], // v0
     -sx / 2,
     -sy / 2,
     sz / 2,
-    0.6,
-    0.7,
-    0.8,
-    0.9, // v2
+    color[16],
+    color[17],
+    color[18],
+    color[19], // v2
     sx / 2,
     -sy / 2,
     sz / 2,
-    1.0,
-    0.9,
-    0.8,
-    0.7, // v3
+    color[20],
+    color[21],
+    color[22],
+    color[23], // v3
 
     sx / 2,
     sy / 2,
     sz / 2,
-    0.6,
-    0.5,
-    0.4,
-    0.3, // v0
+    color[24],
+    color[25],
+    color[26],
+    color[27], // v0
     sx / 2,
     -sy / 2,
     sz / 2,
-    0.2,
-    0.1,
-    0.0,
-    0.3, // v3
+    color[28],
+    color[29],
+    color[30],
+    color[31], // v3
     sx / 2,
     -sy / 2,
     -sz / 2,
-    0.6,
-    0.9,
-    0.7,
-    0.2, // v4
+    color[32],
+    color[33],
+    color[34],
+    color[35], // v4
 
     sx / 2,
     sy / 2,
     sz / 2,
-    0.0,
-    1.0,
-    0.5,
-    0.8, // v0
+    color[36],
+    color[37],
+    color[38],
+    color[39], // v0
     sx / 2,
     -sy / 2,
     -sz / 2,
-    1.0,
-    0.5,
-    0.0,
-    0.2, // v4
+    color[40],
+    color[41],
+    color[42],
+    color[43], // v4
     sx / 2,
     sy / 2,
     -sz / 2,
-    0.5,
-    0.0,
-    1.0,
-    1.0, // v5
+    color[44],
+    color[45],
+    color[46],
+    color[47], // v5
 
     sx / 2,
     sy / 2,
     sz / 2,
-    0.7,
-    0.3,
-    0.8,
-    0.2, // v0
+    color[48],
+    color[49],
+    color[50],
+    color[51], // v0
     sx / 2,
     sy / 2,
     -sz / 2,
-    0.9,
-    0.1,
-    1.0,
-    0.1, // v5
+    color[52],
+    color[53],
+    color[54],
+    color[53], // v5
     -sx / 2,
     sy / 2,
     -sz / 2,
-    0.2,
-    0.5,
-    0.8,
-    0.5, // v6
+    color[56],
+    color[57],
+    color[58],
+    color[59], // v6
 
     sx / 2,
     sy / 2,
     sz / 2,
-    0.1,
-    0.9,
-    0.2,
-    0.8, // v0
+    color[60],
+    color[61],
+    color[62],
+    color[63], // v0
     -sx / 2,
     sy / 2,
     sz / 2,
-    0.3,
-    0.7,
-    0.4,
-    0.6, // v1
+    color[64],
+    color[65],
+    color[66],
+    color[67], // v1
     -sx / 2,
     sy / 2,
     -sz / 2,
-    0.5,
-    0.5,
-    0.6,
-    0.4, // v6
+    color[68],
+    color[69],
+    color[70],
+    color[71], // v6
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    0,
-    1,
-    0,
-    0.5, // v7
+    color[72],
+    color[73],
+    color[74],
+    color[75], // v7
     -sx / 2,
     -sy / 2,
     sz / 2,
-    0,
-    0.5,
-    0.2,
-    0.5, // v2
+    color[76],
+    color[77],
+    color[78],
+    color[78], // v2
     sx / 2,
     -sy / 2,
     sz / 2,
-    0,
-    0,
-    1,
-    0.9, // v3
+    color[80],
+    color[81],
+    color[82],
+    color[83], // v3
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    0.5,
-    0.1,
-    0.5,
-    0.4, // v7
+    color[84],
+    color[85],
+    color[86],
+    color[87], // v7
     sx / 2,
     -sy / 2,
     sz / 2,
-    0.5,
-    0.3,
-    0.5,
-    0.5, // v3
+    color[88],
+    color[89],
+    color[90],
+    color[91], // v3
     sx / 2,
     -sy / 2,
     -sz / 2,
-    0.5,
-    0.5,
-    0.1,
-    0.5, // v4
+    color[92],
+    color[93],
+    color[94],
+    color[95], // v4
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    0,
-    0,
-    1,
-    0.5, // v7
+    color[96],
+    color[97],
+    color[98],
+    color[99], // v7
     -sx / 2,
     sy / 2,
     sz / 2,
-    0,
-    0,
-    0.4,
-    1, // v1
+    color[100],
+    color[101],
+    color[102],
+    color[103], // v1
     -sx / 2,
     -sy / 2,
     sz / 2,
-    0.5,
-    0,
-    0.5,
-    0.2, // v2
+    color[104],
+    color[105],
+    color[106],
+    color[107], // v2
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    0.8,
-    0.2,
-    0,
-    1, // v7
+    color[108],
+    color[109],
+    color[110],
+    color[111], // v7
     -sx / 2,
     sy / 2,
     sz / 2,
-    0,
-    0.8,
-    0.2,
-    1, // v1
+    color[112],
+    color[113],
+    color[114],
+    color[115], // v1
     -sx / 2,
     sy / 2,
     -sz / 2,
-    0.2,
-    0,
-    0.8,
-    1, // v6
+    color[116],
+    color[117],
+    color[118],
+    color[119], // v6
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    1,
-    0,
-    0,
-    1, // v7
+    color[120],
+    color[121],
+    color[122],
+    color[123], // v7
     -sx / 2,
     sy / 2,
     -sz / 2,
-    0,
-    1,
-    0,
-    1, // v6
+    color[124],
+    color[125],
+    color[126],
+    color[127], // v6
     sx / 2,
     sy / 2,
     -sz / 2,
-    0,
-    0,
-    1,
-    1, // v5
+    color[128],
+    color[129],
+    color[130],
+    color[131], // v5
 
     -sx / 2,
     -sy / 2,
     -sz / 2,
-    1,
-    0,
-    0,
-    0, // v7
+    color[132],
+    color[133],
+    color[134],
+    color[135], // v7
     sx / 2,
     sy / 2,
     -sz / 2,
-    0.5,
-    0.2,
-    0.3,
-    0.5, // v5
+    color[136],
+    color[137],
+    color[138],
+    color[139], // v5
     sx / 2,
     -sy / 2,
     -sz / 2,
-    0.5,
-    0.2,
-    0.3,
-    1, // v4
+    color[140],
+    color[141],
+    color[142],
+    color[143], // v4
   ];
 
   return vertexArray;
@@ -469,7 +475,7 @@ function renderScene() {
     Math.sin(rotY),
     0.0,
     Math.cos(rotY),
-    0.0, // For pseudo perspective View
+    0.5, // For pseudo perspective View
     0.0,
     0.0,
     0.0,
